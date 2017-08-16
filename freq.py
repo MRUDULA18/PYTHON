bedf73eb0e89d6af579f68954e62b295c511db7b
MRUDULA18 <31062201+MRUDULA18@users.noreply.github.com>
2017-08-16 20:46:25 +0530

    Update freq.py

--- freq.py
+++ freq.py
@@ -1,10 +1,8 @@
-def char_frequency(str1):
- dict = {}
- for n in str1:
- keys = dict.keys()
- if n in keys:
- dict[n] += 1
- else:
- dict[n] = 1
- return dict
-print(char_frequency('google.com'))
+def countLetters(word):
+    letterdict={}
+    for letter in word:
+        letterdict[letter] = 0
+    for letter in word:
+        letterdict[letter] += 1
+    return letterdict
+ print countLetters("yahoo.in")
